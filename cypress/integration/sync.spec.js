1dd3ac235363451057794700eddfcb3ca4fa04cc
Paulo Ricardo <pricardo.ti@gmail.com>
2021-03-21 21:53:11 -0300

    Atualizando - sync e promisses

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -31,7 +31,8 @@ describe('Esperas', () => {
     })
 
     it('Uso do find', () => {
-        cy.get('#buttonList').click()
+        // cy.get('#buttonList').click()
+        cy.get('#buttonListDOM').click()
         cy.get('#lista li')
             .find('span')
             .should('contain', 'Item 1')
@@ -43,9 +44,75 @@ describe('Esperas', () => {
             .should('contain', 'Item 2')
     })
 
-    it.only('Click Retry', () => {
+    it('Click Retry', () => {
         cy.get('#buttonCount')
             .click()
             .should('have.value', '1')
+    })
+
+    it('Uso do timeout', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('exist')
+
+        // TODO: Configuração especifica para um cenário
+        // cy.get('#novoCampo', {
+        //     timeout: 1000
+        // }).should('exist')
+
+        // TODO: configuração global
+        // Configuração geral da aplicação
+        // Adicionar: "defaultCommandTimeout": 10000
+        // no arquivo cypress.json
+
+        cy.get('#buttonListDOM').click()
+        // para a execução e espera o tempo definido
+        // cy.wait(5000)
+
+        cy.get('#lista li span')
+            .should('have.length', 1)
+            .should('contain', 'Item 1')
+
+        cy.get('#lista li span')
+            .should('have.length', 2)
+            .should('contain', 'Item 2')
+    })
+
+    it.only('Should vs Then', () => {
+        cy.get('#buttonListDOM').click()
+        // TODO: O then aguarda a conclusão da execução
+        // o should tem o processo de sincronização do cypress e realiza verificações com frequencia;
+        // Obs.: fica executando o retry;
+        cy.get('#lista li span').then($el => {
+            expect($el).to.have.length(1)
+        })
+
+        // TODO: o Should ignora o retorno da função;
+        // o Then se não informar o return ele retornar o mesmo objeto, caso informe será considerado o return;
+        cy.get('#buttonListDOM')
+            .then($el => {
+                expect($el).to.have.length(1)
+                return 2;
+            })
+            .and('eq', 2)
+        cy.get('#buttonListDOM')
+            .then($el => {
+                expect($el).to.have.length(1)
+            })
+            .and('have.id', 'buttonListDOM')
+
+        // TODO: diferença should vs then em busca aninhadas de elementos
+        // should com o retry fica em loop - ou seja use o 'then'
+        cy.get('#buttonListDOM')
+            .then($el => {
+                expect($el).to.have.length(1)
+                cy.get('#buttonList')
+            })
+            
+        // --- CUIDADO NESTES CENARIOS ---
+        // cy.get('#buttonListDOM')
+        //     .should($el => {
+        //         expect($el).to.have.length(1)
+        //         cy.get('#buttonList')
+        //     })
     })
 })
